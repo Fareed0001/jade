f9ae74fe94b12624c84c078f8091b30baf4b1079
Farid Ibrahim <fareedibrahim0003@gmail.com>
2022-12-25 00:12:01 +0100

    break points

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-if($(window).width()<480){$(".jade-div, .name-div").removeClass("col-md-3").addClass("col-6");$(".class-image-div").css("display","none");$(".forth-icon").removeClass("fa-2x").addClass("fa-lg");$(".about-div, .connect-div").removeClass("col-md-4").addClass("col-6");$("").removeClass("col-md-2").addClass("col-6");footer-first-col1
-$(".education-div").removeClass("col-md-8").addClass("col-12");$(".competence-div").removeClass("col-md-4").addClass("col-12")}else{$(".class-image-div").css("display","block")}
+  $(".first-div").fadeOut();
+
+  $(".first-div").fadeIn(2000);
+
+
+if ($(window).width() < 480) {
+  $(".jade-div").removeClass("col-md-3").addClass("col-12");
+  $(".class-image-div").css("display", "none");
+  $(".forth-icon").removeClass("fa-2x").addClass("fa-lg");
+  $(".about-div").removeClass("col-md-4").addClass("col-5");
+  $(".connect-div").removeClass("col-md-4").addClass("col-7");
+  $(".education-div").removeClass("col-md-8").addClass("col-12");
+  $(".competence-div").removeClass("col-md-4").addClass("col-12")
+} else {
+  $(".class-image-div").css("display", "block")
+}
